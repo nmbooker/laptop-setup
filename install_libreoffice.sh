6dcc9bd82c2da2270c67f9447e091ad0b5aaa0d8
Barry <barry@merlin.(none)>
2012-05-17 19:34:57 +0100

    Specify implementation of libgl1 as libgl1-mesa-glx

--- install_libreoffice.sh
+++ install_libreoffice.sh
@@ -2,7 +2,7 @@
 
 agy="apt-get -y"
 
-$agy install libgl1  cups-bsd libxrender1 imagemagick pstoedit libpaper-utils libreoffice-filter-binfilter
+$agy install libgl1-mesa-glx  cups-bsd libxrender1 imagemagick pstoedit libpaper-utils libreoffice-filter-binfilter
 $agy libreoffice libreoffice-l10n-en-gb libreoffice-help-en-gb libreoffice-gtk
 $agy install openclipart-libreoffice
 $agy install myspell-en-gb mythes hyphen-en-us
