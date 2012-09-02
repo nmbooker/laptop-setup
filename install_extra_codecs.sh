8172634d93216f1e65d04c8266223e206b34c141
Nick <nick@oak.(none)>
2012-09-02 14:12:36 +0100

    Added libav-tools, and comments about AAC format.

--- install_extra_codecs.sh
+++ install_extra_codecs.sh
@@ -2,4 +2,7 @@
 
 agy="apt-get -y"
 
-$agy install libavcodec-extra-53
+
+# libav-tools contains ffmpeg command line
+# libavcodec-extra-53 contains, among other things, support for Apple AAC files
+$agy install libavcodec-extra-53 libav-tools
