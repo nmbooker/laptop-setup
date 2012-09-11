6101dced5ac68e5dfec4c963318b45af80d54ed7
root <root@hex.(none)>
2012-09-11 17:26:31 +0100

    Modified sfd2012.sh slightly to remove unnecessary bits.

--- sfd2012.sh
+++ sfd2012.sh
@@ -2,16 +2,16 @@
 
 # From a base of Linux Mint 13 Cinnamon Edition.
 # This is my script for setting up an environment for people to play with
-# at the Rugby event on Software Freedom Day 2012.
+# on my laptop at the Rugby event on Software Freedom Day 2012.
 
 apt-get update
-apt-get upgrade
+apt-get dist-upgrade
 
 ./install_gvim.sh
+apt-get -y install idle
 ./install_aptitude.sh
 ./install_edu_games.sh
 ./install_games.sh
-./install_java_plugin.sh
 ./install_mscorefonts.sh
 ./big/install_cheese.sh
 ./devel/install_git.sh
